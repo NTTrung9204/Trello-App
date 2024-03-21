54d1a6e29a66d5fde5fc3b3d3893d107f0d77baa
NTTrung9204 <trung9204@gmail.com>
2024-03-21 23:51:48 +0700

    feat: create button

--- src/JSComponent/BackGround.js
+++ src/JSComponent/BackGround.js
@@ -1,17 +1,24 @@
 import { useState, useEffect } from 'react';
 import TodoApp from './TodoApp/TodoApp.js';
 
+function generateTimestampID() {
+    return Date.now().toString(36);
+}
+
 var isClicked = false;
 
 document.body.addEventListener('mouseup', function (e) {
-    if (isClicked) {
-        isClicked = false;
-    }
+    isClicked = false;
+    const todoApp = document.getElementsByClassName("todoApp");
+    Array.from(todoApp).forEach(element => {
+        element.style.transition = 'all 0.3s';
+    });
 });
 
 function BackGround() {
     useEffect(() => {
         const todoApp = document.getElementsByClassName("todoApp");
+        var zIndexCurrent = 0;
 
         var positionPointer = { x: 0, y: 0 };
 
@@ -19,7 +26,7 @@ function BackGround() {
             element.addEventListener('mousedown', function (event) {
                 isClicked = true;
                 element.style.transition = 'all 0.0s';
-                element.style.zIndex = 100;
+                element.style.zIndex = ++zIndexCurrent;
 
                 positionPointer.x = event.clientX - element.getBoundingClientRect().left;
                 positionPointer.y = event.clientY - element.getBoundingClientRect().top;
@@ -41,6 +48,9 @@ function BackGround() {
             <TodoApp data_id={123} />
             <TodoApp data_id={333} />
             <TodoApp data_id={321} />
+            <button id="createTodo">
+            <i className="fas fa-plus"></i>
+            </button>
         </div>
     );
 }
