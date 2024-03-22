8815e75aad215c658af5405af787abad2c3d3395
NTTrung9204 <trung9204@gmail.com>
2024-03-22 20:26:22 +0700

    feat: hiden and show todoApp

--- src/JSComponent/TodoApp/HeaderListTask.js
+++ src/JSComponent/TodoApp/HeaderListTask.js
@@ -1,8 +1,22 @@
+import { useEffect } from "react";
+
 export default function HeaderListTask({todoAppList}){
     const nameBoardList = []
     todoAppList.forEach((data_id) => {
         nameBoardList.push(JSON.parse(localStorage.getItem(data_id + "_nameBoard")));
     });
+
+    useEffect(() => {
+        const HeaderItems = document.getElementsByClassName("headerItem")
+        Array.from(HeaderItems).forEach((headerItem, index) => {
+            headerItem.addEventListener('click', function(event){
+                const hidenTodoApp = document.getElementsByClassName("todoApp")[index];
+                hidenTodoApp.classList.toggle("todoApp__SwitchHiden");
+                headerItem.classList.toggle("headerItem__SwitchActive");
+            });
+        });
+
+    }, []);
 
     return(
         <div id="headerListTask">
